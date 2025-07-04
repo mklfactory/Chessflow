7450b6e401bff2f5f62b71e833ca96f95e5d0cd9
mklfactory <mklfactory@gmail.com>
2025-07-04 17:45:09 +0200

    updateplayer .py

--- models/player.py
+++ models/player.py
@@ -1,16 +1,27 @@
 class Player:
-    def __init__(self, chess_id, last_name, first_name, birthdate):
-        self.chess_id = chess_id
+    def __init__(self, national_id, last_name, first_name, birth_date):
+        self.national_id = national_id
         self.last_name = last_name
         self.first_name = first_name
-        self.birthdate = birthdate
+        self.birth_date = birth_date
+        self.score = 0
 
     def to_dict(self):
-        return self.__dict__
+        return {
+            "national_id": self.national_id,
+            "last_name": self.last_name,
+            "first_name": self.first_name,
+            "birth_date": self.birth_date,
+            "score": self.score,
+        }
 
     @staticmethod
     def from_dict(data):
-        return Player(**data)
-
-    def __str__(self):
-        return f"{self.first_name} {self.last_name} ({self.chess_id})"
+        player = Player(
+            data["national_id"],
+            data["last_name"],
+            data["first_name"],
+            data["birth_date"]
+        )
+        player.score = data.get("score", 0)
+        return player
