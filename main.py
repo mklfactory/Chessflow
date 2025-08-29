e8892fe420838c6c3bcf15424c3ae31f49d13a73
mklfactory <mklfactory@gmail.com>
2025-08-29 11:43:45 +0200

    updta main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from controllers.main_controller import MainController
 
 def main():
+    print("[DEBUG] Starting ChessFlow Console App...")
     app = MainController()
     app.run()
 
 if __name__ == "__main__":
     main()
-1
