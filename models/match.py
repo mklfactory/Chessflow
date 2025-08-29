504b1b23fdfdd306544cdf5ba7f48f276a55824a
mklfactory <mklfactory@gmail.com>
2025-08-29 16:48:16 +0200

    update match.py

--- models/match.py
+++ models/match.py
@@ -1,20 +1,17 @@
-class Match:
-    def __init__(self, player1=None, player2=None, score1=0.0, score2=0.0):
-        self.player1 = player1
-        self.player2 = player2
-        self.score1 = float(score1)
-        self.score2 = float(score2)
+from models.base_model import BaseModel
 
-    def to_list(self):
-        return [[self.player1.id if self.player1 else None, self.score1],
-                [self.player2.id if self.player2 else None, self.score2]]
+class Match(BaseModel):
+    def __init__(self, player1_id, player2_id, score1=0.0, score2=0.0, match_id=None):
+        super().__init__(match_id)
+        self.player1_id = player1_id
+        self.player2_id = player2_id
+        self.score1 = score1
+        self.score2 = score2
 
-    @staticmethod
-    def from_list(data):
-        # data: [[player1_id, score1], [player2_id, score2]]
-        from models.player import Player
-        p1 = Player.load_by_id(data[0][0]) if data[0][0] else None
-        p2 = Player.load_by_id(data[1][0]) if data[1][0] else None
-        s1 = float(data[0][1])
-        s2 = float(data[1][1])
-        return Match(p1, p2, s1, s2)
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "player1_id": self.player1_id,
+            "player2_id": self.player2_id,
+            "score1": self.score1,
+            "score2": self.score2,
