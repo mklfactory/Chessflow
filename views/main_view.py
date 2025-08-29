ae82fad6e9af70a347ec29bb7f95f5f2ad9b709c
mklfactory <mklfactory@gmail.com>
2025-08-29 11:53:31 +0200

    update main_view.py

--- views/main_view.py
+++ views/main_view.py
@@ -4,8 +4,9 @@ class MainView:
 
     def display_main_menu(self):
         print("\n=== ChessFlow Console App ===")
-        print("1. Gérer les tournois")
-        print("2. Gérer les rounds")
-        print("3. Gérer les joueurs")
+        print("1. Gestion des tournois")
+        print("2. Gestion des rounds")
+        print("3. Gestion des joueurs")
+        print("4. Rapports")
         print("0. Quitter")
         return input("Choisissez une option : ")
