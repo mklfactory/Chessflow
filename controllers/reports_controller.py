274a6ab09d2fdaea7ed4ac1d3d5881402de0f141
mklfactory <mklfactory@gmail.com>
2025-09-04 12:28:28 +0200

    refactor: rename méthods for the best quality and symplifi round and tournament gestion

--- controllers/reports_controller.py
+++ controllers/reports_controller.py
@@ -2,6 +2,7 @@ from models.tournament import Tournament
 from models.player import Player
 from views.reports_view import ReportsView
 
+
 class ReportsController:
     def __init__(self, interface):
         self.view = ReportsView(interface)
@@ -10,51 +11,43 @@ class ReportsController:
         while True:
             choice = self.view.display_menu()
             if choice == "1":
-                self.list_all_players()
+                self.all_players_alpha()
             elif choice == "2":
-                self.list_all_tournaments()
+                self.all_tournaments()
             elif choice == "3":
-                self.tournament_info()
-            elif choice == "4":
                 self.tournament_players_alpha()
-            elif choice == "5":
-                self.tournament_rounds_matches()
+            elif choice == "4":
+                self.tournament_rounds_and_matches()
             elif choice == "0":
                 break
-            else:
-                self.view.show_message("Choix invalide.")
 
-    def list_all_players(self):
+    def all_players_alpha(self):
         players = Player.load_all()
-        self.view.show_players(Player.sort_alphabetically(players))
+        players_sorted = Player.sort_alphabetically(players)
+        self.view.show_players(players_sorted)
 
-    def list_all_tournaments(self):
+    def all_tournaments(self):
         tournaments = Tournament.load_all()
         self.view.show_tournaments(tournaments)
 
-    def tournament_info(self):
-        t = self._get_tournament_or_msg()
-        if t:
-            self.view.show_tournament_info(t)
-
     def tournament_players_alpha(self):
-        t = self._get_tournament_or_msg()
-        if t:
-            self.view.show_players(Player.sort_alphabetically(t.players))
-
-    def tournament_rounds_matches(self):
-        t = self._get_tournament_or_msg()
-        if not t:
-            return
-        for r in t.rounds:
-            self.view.show_round_summary(r)
-            for i, m in enumerate(r.matches):
-                self.view.show_match_detail(i+1, m)
-
-    def _get_tournament_or_msg(self):
-        tid = self.view.ask_tournament_id()
-        t = Tournament.load_by_id(tid)
+        tournament_id = self.view.ask_tournament_id()
+        t = Tournament.load_by_id(tournament_id)
         if not t:
             self.view.show_message("Tournoi introuvable.")
-            return None
-        return t
+            return
+        players = [Player.load_by_id(pid) for pid in t.player_ids]
+        players = [p for p in players if p]
+        players_sorted = Player.sort_alphabetically(players)
+        self.view.show_players(players_sorted)
+
+    def tournament_rounds_and_matches(self):
+        tournament_id = self.view.ask_tournament_id()
+        t = Tournament.load_by_id(tournament_id)
+        if not t:
+            self.view.show_message("Tournoi introuvable.")
+            return
+        for r in t.get_rounds():
+            self.view.show_round_summary(r)
+            for i, m in enumerate(r.get_matches()):
+                self.view.show_match_detail(i + 1, m)
